70f9d26ff9bc2dbbadf81159e2bdbfa2c1fa7aec
dholubcode <55968383+dholubcode@users.noreply.github.com>
2021-01-21 13:50:32 -0800

    Update introHCI.js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,17 +2,33 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
-})
+    initializePage();
+    projectClick();
+});
 
-/*
- * Function that is called when the document is ready.
- */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $("#testjs").click(function(e) {
+        $('.jumbotron h1').text("Javascript is connected");
+        $(this).text("Too many questions is the Chan disease.");
+        $(".jumbotron p").toggleClass("active");
+    });
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+    // Add any additional listeners here
+    // example: $("#div-id").click(functionToCall);
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    console.log("CLICK1");
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+    //var containingProject = $(this).closest(".project");
+    //containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
 }
